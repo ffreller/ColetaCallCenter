32eab3bb893f15ae4e05c7ca99eb0aa988697b9a
Fabio Freller <f.freller@gmail.com>
2022-06-02 23:22:53 +0000

    day 1

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,24 +1,11 @@
 import pandas as pd
 import re
-import calendar
 from os import remove as os_remove
 from io import StringIO
 from html.parser import HTMLParser
-from math import isnan
-from datetime import datetime
+from datetime import datetime, timedelta
 from striprtf.striprtf import rtf_to_text
 from src.definitions import PROCESSED_DATA_DIR
-
-
-def my_dateparser(date_str):
-    if type(date_str) == float:
-        if isnan(date_str):
-            return pd.to_datetime('NaT')
-    elif len(date_str) == 10:
-        return datetime.strptime(date_str, '%d/%m/%Y')
-    elif len(date_str) < 19:
-        return datetime.strptime(date_str, '%d/%m/%Y %H:%M')
-    return datetime.strptime(date_str, '%d/%m/%Y %H:%M:%S')
 
 
 def campo_sepse_med(text):
@@ -151,40 +138,20 @@ def apply_rtf_and_bold_expression(text, all_expressions):
     return new_text 
 
 
-def hemocultura_antibiotico_dentro_do_periodo(row, horarios_sepse_dict):
-    n_atend = row['NR_ATENDIMENTO']
-    data = row['DT_LIBERACAO']
-    data_sepse = horarios_sepse_dict[n_atend]
-    diff_horas = (data - data_sepse).total_seconds()/3600
-    return round(diff_horas, 2)
+def get_excel_fpath():
+    start_day, end_day = get_start_and_end_day()
+    fpath = PROCESSED_DATA_DIR/ f"{start_day.strftime('%d/%m/%Y')}_{end_day.strftime('%d/%m/%Y')}_callcenter.xlsx"
+    return fpath
 
 
-def delete_month_files():
-    selecionados_fn_paulista, selecionados_fn_vergueiro = get_selecionados_fn_for_month()
-    os_remove(selecionados_fn_paulista)
-    os_remove(selecionados_fn_vergueiro)
+def delete_week_file():
+    fpath = get_excel_fpath()
+    os_remove(fpath)
     
     
-def get_last_month_and_year():
+def get_start_and_end_day():
     today = datetime.today()
-    target_month, target_year = today.month - 1, today.year
-    if target_month == 0:
-        target_month = 12
-        target_year -= 1
-    return target_month, target_year, today.month, today.year
-
-
-def get_selecionados_fn_for_month(unidade='ambas'):
-    assert unidade.lower() in ['ambas', 'paulista', 'vergueiro'], print("Unidade precisa ser 'ambas', 'paulista' ou 'vergueiro'")
-    target_month, year_of_target_month, _, _ = get_last_month_and_year()
-    target_date = datetime(year=year_of_target_month, month=target_month, day=1)
-    if unidade.lower() == 'ambas' or unidade.lower() == 'paulista':
-        selecionados_fn_paulista = PROCESSED_DATA_DIR / f"Selecionados_Paulista_{target_date.strftime('%m-%Y')}.xlsx"
-        if unidade.lower() == 'paulista':
-            return selecionados_fn_paulista
-    if unidade.lower() == 'ambas' or unidade.lower() == 'vergueiro':
-        selecionados_fn_vergueiro = PROCESSED_DATA_DIR / f"Selecionados_Vergueiro_{target_date.strftime('%m-%Y')}.xlsx"
-        if unidade.lower() == 'vergueiro':
-            return selecionados_fn_vergueiro
-    return selecionados_fn_paulista, selecionados_fn_vergueiro
+    end_day = today - timedelta(days=1)
+    start_day = today - timedelta(days=7) 
+    return start_day, end_day
     
